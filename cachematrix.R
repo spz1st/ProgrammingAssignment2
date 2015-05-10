9669e415daf93ef1a8184b6faec62acb4a97eed7
zhang <spz1st@gmail.com>
2015-05-10 16:33:20 -0400

    revised some comments

--- cachematrix.R
+++ cachematrix.R
@@ -1,30 +1,26 @@
 ## Data science course assignment 2
 
-## create a special object which can cache the inverse of the given matrix
-## the problem is how to avoid name clashes
+## create a special object from a matrix
+## so the inverse of the matrix can be cached
 
 makeCacheMatrix <- function(x = matrix()) {
-  im <- NULL  # will get error first time call getinv() if not set
-  # cached.m <<- x
+  im <- NULL  # will get error first time call of getinv() if not set
 
-  set <- function(y) {
+  set <- function(y) {  # needed to change the matrix
      x <<- y
      im <<- NULL
   }
-  get <- function() x
-  getinv <- function() im
+  get <- function() x   # x is saved in the object as the local variable
+  getinv <- function() im  # im is saved in the object as the local variable
   setinv <- function(inv) im <<- inv
   list(set = set, get = get, setinv = setinv, getinv = getinv)
 }
 
-
 ## get the inverse of the matrix object created by makeCacheMatrix
-## if the inverse was already made and the matrix was not changed
-## just return the cached inverse. Otherwise compute the inverse
-## and cache it before returning it.
+## if the inverse was already made, just return the cached inverse.
+## Otherwise compute the inverse and cache it before returning it.
 
-cacheSolve <- function(x, ...) {
-  ## Return a matrix that is the inverse of 'x'
+cacheSolve <- function(x, ...) { ## Return a matrix that is the inverse of 'x'
   im <- x$getinv()
   if(!is.null(im)){
     message("return cached inverse")
